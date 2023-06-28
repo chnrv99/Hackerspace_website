6a0aa3053905c72aed32eb6021d984da9f666bc7
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-06-28 11:44:00 +0530

    io

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,7 +2,6 @@ import React, { Suspense, useEffect, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF, useAnimations } from "@react-three/drei";
 import './App.css';
-import Scene from "./Scene";
 import Navbar from '../components/navBar/nav'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Speaker from '../components/speaker/Speakers'
